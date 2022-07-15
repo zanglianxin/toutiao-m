fb9a47c3f501f41f1199fbe483f30486a0614e75
fengbujue <1125238900@qq.com>
2022-07-15 17:57:53 +0800

    完成了layout布局

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,29 @@ const routes = [
   {
     path: '/login',
     component: () => import('@/views/Login') // 路由懒加载，需要哪个页面加载哪个页面
+  },
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: '/qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/profile',
+        component: () => import('@/views/My')
+      }
+    ]
   }
 ]
 
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <span>首页</span>
+        <template #icon>
+          <span class="toutiao toutiao-shouye"></span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <span>视频</span>
+        <template #icon>
+          <span class="toutiao toutiao-shipin"></span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <span>问答</span>
+        <template #icon>
+          <span class="toutiao toutiao-wenda"></span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/profile">
+        <span>我的</span>
+        <template #icon>
+          <span class="toutiao toutiao-wode"></span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+:deep(.van-tabbar-item) {
+  font-size: 0.32rem;
+  .toutiao {
+    font-size: 0.5333rem
+  }
+}
+</style>
